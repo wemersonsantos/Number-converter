360fa76907b11af14f8157e73d02d0dbb561caf4
wemersonsantos <wemersonsantoseng@gmail.com>
2021-06-09 21:27:47 -0300

    adicinando estilo

--- js/script.js
+++ js/script.js
@@ -2,14 +2,28 @@ let numberforConv = document.querySelector("#number");
 const btn = document.querySelector("#buttonSb");
 let numberConv = document.querySelector("#numberConv");
 
-function bin2dec(bin){
-    let decimal = 0;
-    for(let index = bin.length -1; index >= 0; index--){
-        decimal += parseInt(bin[index])*Math.pow(2, bin.length-1-index);
-    }
-    return decimal;
-}
+    function bin2dec(bin){
+        let decimal = 0;
+        for(let index = bin.length -1; index >= 0; index--){
+            decimal += parseInt(bin[index])*Math.pow(2, bin.length-1-index);
+        };
+        return decimal;
+    };
 
-btn.addEventListener("click" , e => {
-    numberConv.value = `${bin2dec(numberforConv.value)}`
-})
+        
+    btn.addEventListener("click" , e => { 
+        let arrayNumber;
+        for(let index = numberforConv.value.length -1; index >= 0; index--){
+            arrayNumber = numberforConv.value.split(""); 
+        };
+
+        arrayNumber.forEach(numb => {
+            if(numb == 1 || numb == 0){
+                numberConv.value = `${bin2dec(numberforConv.value)}`;  
+            }else{
+                numberConv.value = 'Valor incorreto'; 
+            };
+        }); 
+
+    });
+   
